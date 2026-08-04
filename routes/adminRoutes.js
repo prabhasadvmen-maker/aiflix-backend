88990a50246d949f20fd01a634876c687dd3bb29
Brahmakosh Team <brahmakoshtech@gmail.com>
2026-08-04 09:51:34 +0530

    aiflix code update backend

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const { loginAdmin } = require("../controllers/authAdminController");
+const { protectAdmin } = require("../middleware/authMiddleware");
+const { getCreators, approveCreator, rejectCreator, deleteCreator } = require("../controllers/creatorManageController");
 
 router.post("/login", loginAdmin);
+
+// Creator Management (Admin access)
+router.get("/creators", protectAdmin, getCreators);
+router.put("/creators/:id/approve", protectAdmin, approveCreator);
+router.put("/creators/:id/reject", protectAdmin, rejectCreator);
+router.delete("/creators/:id", protectAdmin, deleteCreator);
 
 module.exports = router;
